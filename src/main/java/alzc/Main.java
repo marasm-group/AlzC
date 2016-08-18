4dc7e5b81301cf7be6d28f2975a9d5ed1f0777fe
Sergey Rump (SPHERE) <Sergey.Rump@motorolasolutions.com>
2016-08-18 13:49:26 +0300

    Initial commit

---
+++ src/main/java/alzc/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by vhq473 on 18.08.2016.
+ */
+package alzc;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
